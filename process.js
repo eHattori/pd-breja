4ac2754de8ca4e41e39f59615789b04e0d8d0459
Eduardo Hattori <eduardo.hattori@hotmail.com>
2017-09-30 23:42:42 -0300

    Added the process manager

---
+++ process.js
@@ -0,0 +1,38 @@
+require('babel-register');
+
+const pm2 = require("pm2");
+
+process.env.PORT = normalizePort(process.env.PORT || 3000)
+
+pm2.connect(function() {
+    pm2.start({
+        script: './src/app.js', 
+        instances: 4,
+        max_memory_restart: '150M', 
+    }, function(err, apps) {
+
+        if (err) {
+            console.log('pm2.connect(app.js)', err);
+            console.trace(err);
+        }
+    });
+});
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
