7c1c41d5499002eabdbfce92419c4f278619b451
Eduardo Hattori <eduardo.hattori@hotmail.com>
2017-10-13 13:12:52 -0300

    Make test to getById in route

--- src/api/pdv/pdv-routes.js
+++ src/api/pdv/pdv-routes.js
@@ -1,7 +1,10 @@
+import PdvController from './pdv-controller';
+
 
 export default class PdvRoutes {
-  constructor (express) {
+  constructor (express, controller) {
     this.express = express;
+    this._controller = !controller ? new PdvController() : controller;    
   }
 
   getApp () {
@@ -11,12 +14,20 @@ export default class PdvRoutes {
             // To-DO post
     });
 
-    app.get(':id', (req, res) => {
-      res.status(404).json({status: 'Pdv not found!'});
+    app.get('/:id', (req, res) => {
+      this._controller.getById(req.params.id, function(result){
+        try {
+            var status =  result.pdvs.length > 0 ? 200 : (result.error ? 400 : 404); 
+            res.status(status).json(result);
+        } catch(error){
+          res.status(500).json(error);
+        }        
+      });
     });
 
-    app.get('', (req, res) => {
-            // To-DO by lng, lat
+    app.post('/', (req, res) => {
+        console.log("XANA 2");
+        res.status(404).json({status: 'Pdv not found!'});
     });
 
     return app;
--- test/routes/pdv-routes.js
+++ test/routes/pdv-routes.js
@@ -1,28 +1,80 @@
 'use strict';
+import PdvController from '../../src/api/pdv/pdv-controller';
 import PdvRoutes from '../../src/api/pdv/pdv-routes';
 import express from 'express';
+import should from 'should';
+import sinon from 'sinon';
 
 describe('Test PdvRoutes', function () {
 
     var pdvApp;
     var request;
-    var should;    
 
     before(function(done){        
 
-        pdvApp = new PdvRoutes(express);
-        should = require('should');
+        pdvApp = new PdvRoutes(express, new PdvController({}));
         request = require('supertest')(pdvApp.getApp());
         done();
     });
 
     it('Should return status 404 when send a ID that not exist', function(done){
-        //TO-DO: mock the controller to return null
+        
+        var stubController = sinon.stub(pdvApp._controller, 'getById').callsFake(function(id, callback){            
+            callback({ pdvs : []});
+        });
+
         request.get('/1')
             .expect(404)
             .end(function(err, res) {
+                stubController.restore();
                 (res.statusCode == 404).should.be.true();
                 done(err);
         });
     });
+
+    it('Should return status 200 when find a PDV with id', function(done){
+        
+        var stubController = sinon.stub(pdvApp._controller, 'getById').callsFake(function(id, callback){            
+            callback({ pdvs : [ { id: "1" }]});
+        });
+
+        request.get('/1')
+            .expect(200)
+            .end(function(err, res) {
+                stubController.restore();
+                (res.statusCode == 200).should.be.true();                
+                done(err);
+        });
+    });
+
+
+    it('Should return status 400 when find a PDV with some error', function(done){
+        
+        var stubController = sinon.stub(pdvApp._controller, 'getById').callsFake(function(id, callback){            
+            callback({ pdvs : [], error : [ 'SOME ERROR']});
+        });
+
+        request.get('/1')
+            .expect(400)
+            .end(function(err, res) {
+                stubController.restore();
+                (res.statusCode == 400).should.be.true();                
+                done(err);
+        });
+    });
+
+
+    it('Should return status 500 when occur some error', function(done){
+        
+        var stubController = sinon.stub(pdvApp._controller, 'getById').throws("Exception");
+
+        request.get('/1')
+            .expect(500)
+            .end(function(err, res) {
+                stubController.restore();
+                (res.statusCode == 500).should.be.true();                
+                done(err);
+        });
+    });
+
 });
